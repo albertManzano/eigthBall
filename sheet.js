400bad008004cd7975e747fa5b7e4c4f282b5655
albertManzano <vetu87@gmail.com>
2019-11-07 19:49:35 +0100

    css beautified

--- sheet.js
+++ sheet.js
@@ -2,36 +2,53 @@
 //logica// 
 var display = document.getElementById('display')
 var answer= document.getElementById('answer')
-
+var ball= document.getElementById('ball')
+var shadow= document.getElementById('shadow')
+var animationRuning = false
 
 function generateAnswer() {
+    if(animationRuning) return
     
-    display.valueL=''
+    display.value=''
     var response;
-    let number = Math.floor(Math.random() * 5)
+    var number = Math.floor(Math.random() * 5)
+    ball.classList.add('rotateball');
+    shadow.classList.add('jump');
+
     switch (number) {
         case 0:
-            response='yes'
+            response='Yes'
             break
         case 1:
-            response='no'
+            response='My reply is no'
             break
         case 2:
-            response='maybe, try your luck'
+            response='It is decidedly so'
             break
         case 3:
-            response='can/t see clearly the future'
+            response='Without a doubt'
             break
         case 4:
+            response='Yes - definitely'
+            break
+        case 5:
             response='wish you the best of lucks trying'
             break
         default:
             response='something went terribly wrong'
             break
     }
+    animationRuning = true
+
+    setTimeout(function() {
+        ball.classList.remove('rotateball');
+        shadow.classList.remove('jump');
+        display.value=response
+        animationRuning = false
+    }, 2000)
     
-    display.value=response
 }
+
 
 answer.addEventListener('click',generateAnswer)
 
